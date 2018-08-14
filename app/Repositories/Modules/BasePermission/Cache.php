cdadcde64b56f067ecc281bb55d9356618d93d17
luffyzhao113 <346838352@qq.com>
2018-08-14 17:57:56 +0800

    修改bug

--- app/Repositories/Modules/BasePermission/Cache.php
+++ app/Repositories/Modules/BasePermission/Cache.php
@@ -12,34 +12,4 @@ class Cache extends CacheAbstractDecorator implements Interfaces
         $this->repo = $repo;
         $this->cache = $cache;
     }
-
-    /**
-     * 通过where条件查找一个模型.
-     *
-     * @method findWhere
-     *
-     * @param array $attributes Where条件
-     * @param array $columns    获取字段
-     *
-     * @return Illuminate\Support\Collection|static|null
-     *
-     * @author luffyzhao@vip.126.com
-     */
-    public function findWhere(array $attributes, array $columns = ['*'])
-    {
-        $cacheKey = $this->getCache('findWhere', [
-            $attributes,
-            $columns,
-        ]);
-
-        if (!is_string($cacheKey)) {
-            return $cacheKey;
-        }
-
-        $model = $this->repo->findWhere($attributes, $columns);
-
-        $this->cache->put($cacheKey, $model);
-
-        return $model;
-    }
 }
